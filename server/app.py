a7a07484eb6ce1645b3d1c25a8546fab74adca7e
abbi portwood <apwood09@gmail.com>
2026-05-25 10:39:51 -0400

    add blog paywall to track page/article views

--- server/app.py
+++ server/app.py
@@ -27,8 +27,45 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-    pass
+    # track & increment page views (cookie/session)
+    # check page_views exists as key in user's session data
+    if 'page_views' not in session: 
+        # first time visitng: key won't exist
+        session['page_views'] = 0 # initial 0 value 
+    
+    # page loads: add 1 to current count
+    # new user: 0 -> 1 very first page load 
+    session['page_views'] += 1 # increment by 1 
 
+    # pageview limit (security & limit check)
+    if session['page_views'] > 3:
+        # viewed > 3 pages: JSON response (error message & status code 401(unauthorized))
+        # page view hits 4 
+        return make_response(
+            jsonify({'message': 'Maximum pageview limit reached'}), 
+            401
+        )
+
+    # query db for requested article 
+    # pass limit check: look for article in db 
+    # 'filter_by(id=id)': search article table where column id matches URL variable 
+    # '.first()': grabs single matching row OR return None
+    article = Article.query.filter_by(id=id).first()
+    if not article: 
+        # article none: return error & 404 (Not Found) status code
+        return make_response(
+            jsonify({'error': 'Article not found'}), 
+            404
+        )
+
+    # serialize & return article data
+    # 'ArticleSchema().dump(article)': convert raw db row into python dictionary 
+    serialize_article = ArticleSchema().dump(article)
+    # successful 200 (OK) status code
+    return make_response(
+        jsonify(serialize_article), 
+        200
+    )
 
 if __name__ == '__main__':
     app.run(port=5555)
